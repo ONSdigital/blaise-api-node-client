37035a595a7f20da412bc71e08564aafaf8cd73f
jamie kerr <jamiestephenkerr@gmail.com>
2021-07-23 13:03:33 +0100

    moved mock objects

--- tests/diagnostic.test.js
+++ tests/diagnostic.test.js
@@ -2,7 +2,7 @@ import MockAdapter from "axios-mock-adapter";
 import axios from "axios";
 import 'regenerator-runtime/runtime'
 import BlaiseApiClient from "../src/blaise-api-client";
-import {diagnosticMockOject} from "../mocks/diagnostic-mocks";
+import {diagnosticMockOject} from "../src/mock-objects/diagnostic-mock-objects";
 
 const mock = new MockAdapter(axios, { onNoMatch: "throwException" });
 const blaiseApiUrl = "testUri"
