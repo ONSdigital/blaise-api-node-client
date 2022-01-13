f513bc7d9160a6a01e15b2c81c0fccd3847a5d7e
Sam Bryant <srbry@hotmail.com>
2022-01-13 09:42:58 +0000

    Add all known Outcomes

--- src/interfaces/instrument.ts
+++ src/interfaces/instrument.ts
@@ -58,7 +58,9 @@ interface CaseStatus {
 
 enum Outcome {
     Completed = 110,
+    CompletedNudge = 120,
     Partial = 210,
+    AppointmentMade = 300,
     NonContact = 310,
     HQRefusal = 430,
     NotAvailable = 440,
@@ -66,9 +68,31 @@ enum Outcome {
     SoftRefusal = 461,
     LanguageDifficultiesHeadOffice = 541,
     LanguageDifficultiesInterviewer = 542,
+    WrongNumber = 542,
     DeleteRequestedCompleted = 561,
     DeleteRequestedPartial = 562,
-    Ineligible = 580
+    IneligibleVacant = 540,
+    IneligibleNonResidential = 551,
+    IneligibleInstitution = 560,
+    IneligibleSecondHome = 580,
+    ConcernsWontTakePart = 360,
+    RejectTandCs = 380,
+    LostAccessCide = 373,
+    UnableToComplete = 370,
+    NoInternetAccess = 371,
+    RequestedDifferentMode = 372,
+    NoTraceOfAddress = 510,
+    Under16 = 631,
+    WrongAddress = 640,
+    BrailleRequested = 411,
+    LargePrintRequested = 412,
+    OtherFormat = 413,
+    DeleteRequested = 390,
+    RequestedCopyOfData = 791,
+    ClarificationOnStudyRequested = 792,
+    AssistanceRequested = 793,
+    RequestForContext = 794,
+    QuestionProblem = 795
 }
 
 export type {
