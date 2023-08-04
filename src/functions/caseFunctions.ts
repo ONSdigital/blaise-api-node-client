0267380603c24af597c3ac510cb41061049a3672
westjake <jacob.west@ons.gov.uk>
2023-08-04 12:32:05 +0100

    caseID to caseId

--- src/functions/caseFunctions.ts
+++ src/functions/caseFunctions.ts
@@ -2,12 +2,12 @@ import BlaiseApiClient from "../blaiseApiClient";
 import {CaseResponse, CaseStatus } from "../interfaces/case";
 import { CaseData } from "../types/caseData";
 
-export async function getCase(this: BlaiseApiClient, serverpark: string, questionnaireName: string, caseID: string): Promise<CaseResponse> {
-  return this.get(`api/v2/serverparks/${serverpark}/questionnaires/${questionnaireName}/cases/${caseID}`);
+export async function getCase(this: BlaiseApiClient, serverpark: string, questionnaireName: string, caseId: string): Promise<CaseResponse> {
+  return this.get(`api/v2/serverparks/${serverpark}/questionnaires/${questionnaireName}/cases/${caseId}`);
 }
 
-export async function addCase(this: BlaiseApiClient, serverpark: string, questionnaireName: string, caseID: string, caseFields: CaseData): Promise<CaseResponse> {
-  return this.post(`api/v2/serverparks/${serverpark}/questionnaires/${questionnaireName}/cases/${caseID}`, caseFields);
+export async function addCase(this: BlaiseApiClient, serverpark: string, questionnaireName: string, caseId: string, caseFields: CaseData): Promise<CaseResponse> {
+  return this.post(`api/v2/serverparks/${serverpark}/questionnaires/${questionnaireName}/cases/${caseId}`, caseFields);
 }
 
 export async function getCaseStatus(this: BlaiseApiClient, serverpark: string, questionnaireName: string): Promise<CaseStatus[]> {
