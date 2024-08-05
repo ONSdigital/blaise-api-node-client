03e60a89d71960129c3681305f8bec98db8cdb38
willst1 <thomas.wills@ons.gov.uk>
2024-08-05 10:45:52 +0100

    Added Editing interface and editing enum

---
+++ src/interfaces/editing.ts
@@ -0,0 +1,10 @@
+import { CaseOutcome } from '../enums/caseOutcome';
+import { EditedStatus } from '../enums/editedStatus';
+
+export interface EditingDetails {
+  primaryKey: string,
+  outcome: CaseOutcome,
+  assignedTo: string,
+  editedStatus: EditedStatus, 
+  interviewer: string,
+}
